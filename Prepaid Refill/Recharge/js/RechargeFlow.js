6860355be7556a7a623e0cdece4631b0aac12106
xieyuangang <846356858@qq.com>
2017-11-02 09:45:56 +0800

    Prepaid Refill

---
+++ Prepaid Refill/Recharge/js/RechargeFlow.js
@@ -0,0 +1,172 @@
+$(function () {
+    window.alert = function(name){
+        var iframe = document.createElement("IFRAME");
+        iframe.style.display="none";
+        iframe.setAttribute("src", 'data:text/plain,');
+        document.documentElement.appendChild(iframe);
+        window.frames[0].window.alert(name);
+        iframe.parentNode.removeChild(iframe);
+    };
+    var memberIdL = window.location.href;
+    var href_part = memberIdL.split('?');
+    var hours = href_part[1].split("&")[0];
+    var hoursH = href_part[1].split("&")[1];
+
+    // console.log(hours);
+   // console.log(hoursH);
+    /*if(hoursH==undefined || hours==undefined){
+        alert("暂时无法充值");
+        return false;
+    }*/
+    var LocalL;
+    var moneyL;
+    var BaseURL = 'http://125.69.76.146:12002/server/wsq';
+    //关闭遮罩
+    $("#Close").click(function () {
+        $(".Mask").hide();
+    });
+    $("#InputBox").blur(function () {
+		  var timer = null;
+        var destNo = $("#InputBox").val();
+        var MobileNumber = /^(134|135|136|137|138|139|150|151|152|157|158|159|182|183|184|187|188)[0-9]{8}$/;
+        var UnicomNumber = /^(130|131|132|145|155|156|185|186)[0-9]{8}$/;
+        var telecomNumber = /^(133|153|180|181|189)[0-9]{8}$/;
+        $(".yin").html(destNo);
+        if (destNo.length == "") {
+         timer =  setInterval(function(){
+                alert("请输入手机号码");
+                clearInterval(timer);
+            },510);
+            return false;
+        } else if (destNo.length != 11) {
+            alert("手机号码不正确，请重新输入");
+            $("#InputBox").val("");
+            return false;
+        }
+        if (MobileNumber.test(destNo)) {
+            $(".move").html('中国移动');
+        } else if (UnicomNumber.test(destNo)) {
+            $(".move").html('中国联通');
+        } else if (telecomNumber.test(destNo)) {
+            console.log("这是一个电信号码");
+            $(".move").html('中国电信');
+        }
+        var destNo = $("#InputBox").val();
+        //var date=JSON.stringify({"queryType":queryType,"destNo":destNo});
+        var queryType = '2';
+
+        var date = {queryType: queryType, destNo: destNo};
+        // console.log(date);
+        $.ajax({
+            url: BaseURL + "/queryPhoneInfo",
+            type: 'post',
+            data: date,
+            dataType: "jsonp",
+            // jsonp: "callback",
+            jsonpCallback: "success_jsonpCallback",
+            contentType: 'application/jsonp;charset=utf-8',
+            success: function (data) {	//成功回调函数
+                console.log(data);
+                var Cycle = "";
+                var list = "";
+                Cycle += "<span><b>" + data.body.province + "</b>省<b>" + data.body.city + "</b>市</span><span>" + data.body.userName + "</span>";
+                $(".address").empty().append(Cycle);
+                //console.log(data);
+                if(data.body.msg==0){
+                    alert(data.body.msg)
+                }else{
+                    if (data.body.listAmt && data.body.listAmt.length > 0) {
+                        for (var i = 0; i <= data.body.listAmt.length - 1; i++) {
+                            //console.log(data.body.listAmt[i]);
+                            list += "<li class='liston'><span class='text'>" + data.body.listAmt[i] + "</span><span>售价<b class='vmoney'>**</b> 元</span></li>";
+                        }
+                        $(".list").empty().append(list);
+                    }
+                }
+            },
+            error: function (data) {	//失败回调函数
+                console.log(data.body.msg);
+            }
+        });
+        return false;
+    });
+
+    $(".list").unbind().on("click", ".liston", function () {
+        var destNo = $("#InputBox").val();
+        var productSize = $(this).find(".text").html();
+        var date = {destNo: destNo, productSize: productSize};
+        $.ajax({
+            url: BaseURL + "/getProductPrice",
+            type: 'post',
+            data: date,
+            dataType: "jsonp",
+            jsonpCallback: "success_jsonpCallback",
+            contentType: 'application/jsonp;charset=utf-8',
+            success: function (data) {
+                //console.log(data);
+                var Cycle = "";
+                for (var i = 0; i < data.body.productList.length; i++) {
+                    Cycle += "<li><div style='width: 70%'><p><span>" + data.body.productList[i].price + "元</span><span class='local'>" + data.body.productList[i].productName + "</span></p><p>本地可用,即使生效,月底无效。</p></div><div style='width: 30%'><button class='Button'>充值</button></div></li>"
+                    LocalL = data.body.productList[i].type;
+                    moneyL = data.body.productList[i].price;
+                }
+                $(".flowButton").empty().append(Cycle);
+            },
+            error: function (data) {
+                console.log(data.body.msg)
+            }
+        })
+    });
+
+    $(".flowButton").on('click', 'Button', function () {
+        PwdBox.init('123456','img/pwd_keyboard.png','请输入支付密码','安全支付环境，请放心使用！');
+        PwdBox.show(function(res){
+            if(res.status){
+                //alert('密码正确');
+                // PwdBox.reset();
+            }else{
+                var  password=arguments[0].password;
+                $(".password-box").hide();
+                //var mobileNo ="18261573288" ;
+                var mobileNo = hoursH;
+                var memberId = hours;
+                var password = password;
+                var destNo = $(".yin").html();
+                var chargeType = '2';
+                var productType = LocalL;
+                var productSize = moneyL;
+                var date = {
+                    mobileNo: mobileNo,
+                    memberId: memberId,
+                    password: password,
+                    destNo: destNo,
+                    chargeType: chargeType,
+                    productType: productType,
+                    productSize: productSize
+                };
+                console.log(date);
+                $.ajax({
+                    url: BaseURL + "/charge",
+                    type: 'post',
+                    data: date,
+                    dataType: "jsonp",
+                    jsonpCallback: 'success_jsonpCallback',
+                    contentType: 'application/jsonp;charset=utf-8',
+                    success: function (data) {
+                        //console.log(data.body.code);
+                        $(".MaskL").hide();
+                        if (data.body.code == 1) {
+                            alert("提交成功")
+                        } else {
+                            alert(data.body.msg)
+                        }
+                    },
+                    error: function (data) {
+                        alert(data.body.msg)
+                    }
+                })
+
+            }
+        });
+    });
+});
---
+++ Prepaid Refill/Recharge/js/RechargeRecord.js
@@ -0,0 +1,362 @@
+$(document).ready(function () {
+    window.alert = function(name){
+        var iframe = document.createElement("IFRAME");
+        iframe.style.display="none";
+        iframe.setAttribute("src", 'data:text/plain,');
+        document.documentElement.appendChild(iframe);
+        window.frames[0].window.alert(name);
+        iframe.parentNode.removeChild(iframe);
+    };
+    var calendar = new lCalendar();
+    calendar.init({
+        'trigger': '#demo1',
+        'type': 'date'
+    });
+    var myDate = new Date();
+    //获取当前年
+    var year = myDate.getFullYear();
+    //获取当前月
+    var month = myDate.getMonth() + 1;
+    //获取当前日
+    var date = myDate.getDate();
+    var now = year + '年' + p(month) + "月" + p(date) + "日";
+    $("#demo1").val(now);
+});
+$(function () {
+    var BaseURL = 'http://125.69.76.146:12002/server/wsq';
+    //  var  transDate  =$(this).val().replace(/年/,"").replace(/月/,"").replace(/日/,"");
+    //获取a标签带过来的值
+    var location = window.location.href;
+    var href= location+"";
+    var href_part=href.split('?');
+    var num=(href_part);
+   //console.log(num[1]);
+    //获取时间
+    var myDate = new Date();
+    //获取当前年
+    var year = myDate.getFullYear();
+     //获取当前月
+    var month = myDate.getMonth() + 1;
+    //获取当前日
+    var date = myDate.getDate();
+    var now = year+ p(month) + p(date);
+    // var transDate =now;
+    var mobile=num[1].split("&");
+    //var memberId =mobile[0];
+
+    //alert(JSON.stringify(date));
+    var memberId = 'bwgzydVRnlfJjGmJ';
+    var transDate = "20171026";
+    var date = {transDate: transDate, memberId: memberId};
+    var counter = 0;
+    // 每页展示4个
+    var num = 4;
+    var pageStart = 0, pageEnd = 0;
+    $('.content').dropload({
+        scrollArea: window,
+        domUp: {
+            domClass: 'dropload-up',
+            domRefresh: '<div class="dropload-refresh">↓下拉刷新</div>',
+            domUpdate: '<div class="dropload-update">↑释放更新</div>',
+            domLoad: '<div class="dropload-load"><span class="loading"></span>加载中...</div>'
+        },
+        domDown: {
+            domClass: 'dropload-down',
+            domRefresh: '<div class="dropload-refresh">↑上拉加载更多...</div>',
+            domLoad: '<div class="dropload-load"><span class="loadin"></span>加载中...</div>',
+            domNoData: '<div class="dropload-noData">加载完成...</div>'
+        },
+        loadDownFn: function (me) {
+            $.ajax({
+                url: BaseURL + "/getTransrecords",
+                type: 'post',
+                data: date,
+                dataType: "jsonp",
+                jsonpCallback: "success_jsonpCallback",
+                contentType: 'application/jsonp;charset=utf-8',
+                success: function (data) {
+
+                    var Total = data.body.totalMoney;
+                    $("#Total").html(Total);
+                    var result = '';
+                    counter++;
+                    pageEnd = num * counter;
+                    pageStart = pageEnd - num;
+                    if (data.body.transrecordList.length == '0') {
+                        result += '<h3 style="text-align: center;line-height: 60px;">暂无充值记录...</h3>';
+                        $('.lists').append(result);
+                    } else {
+                        for (var i = pageStart; i < pageEnd; i++) {
+                            var str = data.body.transrecordList[i].createTime;
+                            var time = str.split(" ");
+                            var hours = time[1].split(":")[0];
+                            var appType = data.body.transrecordList[i].appType;
+
+                            var status = data.body.transrecordList[i].status;
+
+                            result += "<li><div style='width:20% '>";
+                            if (hours < 12) {
+                                result += "<span class='morning'>上午</span>";
+                            } else {
+                                result += "<span class='morning'>下午</span>";
+                            }
+                            result += "<span>" + time[1] + "</span></div><div style='width:25% '>";
+                            if (appType == 1000) {
+                                result += "<h4>话费充值</h4>";
+                            } else {
+                                result += "<h4>话费流量</h4>";
+                            }
+                            result += "</div><div style='width:41% '><span>-" + data.body.transrecordList[i].realAmt + "</span> <span style='font-size: 0.9rem;line-height: 1rem;'>给" + data.body.transrecordList[i].destNo + "充值</span></div> "
+                            if (status == 0) {
+                                result += "<div style='width: 7%'><span class='Result' style='color:#999;white-space:nowrap'>充值</br>失败</span></div>";
+                            } else if (status == 1) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>充值</br>成功</span></div>";
+                            } else if (status == 2) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>等待</br>充值</span></div>";
+                            }
+                            if (status == 3) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>充值</br>中</span></div>";
+                            }
+                            if (status == 4) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销</br>审核中</span></div>";
+                            }
+                            if (status == 5) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销中</span></div>";
+                            }
+                            if (status == 6) {
+                                result += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销</br>成功</span></div>";
+                            }
+                            result += "</li>";
+
+                            if ((i + 1) >= data.body.transrecordList.length) {
+                                // 锁定
+                                me.lock();
+                                // 无数据
+                                me.noData();
+                                break;
+                            }
+                        }
+                        // 为了测试，延迟1秒加载
+                        setTimeout(function () {
+                            $('.lists').append(result);
+                            // 每次数据加载完，必须重置
+                            me.resetload();
+                        }, 800);
+                    }
+                },
+                error: function (data) {
+                    alert("服务器请求异常");
+                    // 即使加载出错，也得重置
+                    me.resetload();
+                }
+            });
+        },
+        threshold: 50
+    });
+  /* $.ajax({
+        url: BaseURL + "/getTransrecords",
+        type: 'post',
+        data:date,
+        dataType: "jsonp",
+        jsonpCallback: "success_jsonpCallback",
+        contentType: 'application/jsonp;charset=utf-8',
+        success: function (data) {	//成功回调
+            var Total = data.body.totalMoney;
+            $("#Total").html(Total);
+            var Cycle = "";
+             for(var i=0;i < data.body.transrecordList.length; i++){
+                 var str=data.body.transrecordList[i].createTime;
+                 var  time=str.split(" ");
+                 var hours=time[1].split(":")[0];
+                 var appType=data.body.transrecordList[i].appType;
+                 var status=data.body.transrecordList[i].status;
+                Cycle+="<li><div style='width:20% '>";
+                if(hours<12){
+                      Cycle+="<span>上午</span>"
+                  }else {
+                      Cycle+="<span>下午</span>"
+             }
+             Cycle+="<span>"+time[1]+"</span></div><div style='width:25% '>";
+                     if(appType==1000){
+                         Cycle+="<h4>话费充值</h4></div>";
+                     }else {
+                         Cycle+="<h4>话费充值</h4></div>";
+                     }
+
+                     if(data.body.transrecordList[i].status==1 || data.body.transrecordList[i].status==0){
+                         Cycle+="<div style='width:40% '><span>-"+data.body.transrecordList[i].realAmt+"</span><span style='font-size: 0.9rem;line-height: 1rem;'>给"+data.body.transrecordList[i].destNo+"充值</span></div>"
+
+                     }else {
+                         Cycle+="<div style='width:40% '><span style='opacity: 0;'>00</span><span style='font-size: 0.9rem;line-height: 1rem;'>给"+data.body.transrecordList[i].destNo+"充值</span></div>"
+
+                     }
+             if(status==0){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color:#999'>充值失败</span></div></li>";
+               }else if(status==1){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>充值成功</span></div></li>";
+               }else if(status==2){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>等待充值</span></div></li>";
+               }if(status==3){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>充值中</span></div></li>";
+               }if(status==4){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销审核中</span></div></li>";
+               }if(status==5){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销中</span></div></li>";
+               }if(status==6){
+                   Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销成功</span></div></li>";
+               }
+           }
+            $(".calendarList").empty().append(Cycle);
+        },
+        error: function (data) {	//失败回调函数
+            console.log(data)
+        }
+    });*/
+    $('#demo1').bind('input propertychange', function () {
+        $(".calendarListY").empty();
+        var dateL = $(this).val().replace(/年/, "").replace(/月/, "").replace(/日/, "");
+        // console.log(date);
+        //var memberId =num[1];
+        var memberId = 'bwgzydVRnlfJjGmJ';
+        var transDate = dateL;
+        var date = {transDate: transDate, memberId: memberId};
+        $(".content").hide();
+        $(".contentY").show();
+        $.ajax({
+            url: BaseURL + "/getTransrecords",
+            type: 'post',
+            data: date,
+            dataType: "jsonp",
+            jsonpCallback: "success_jsonpCallback",
+            contentType: 'application/jsonp;charset=utf-8',
+            success: function (data) {	//成功回调函数
+                var Cycle = "";
+                if (data.body.transrecordList.length == '0') {
+                    Cycle+= '<h3 style="text-align: center;line-height: 60px;">暂无充值记录...</h3>';
+                    $('.calendarListY').append(Cycle);
+                } else {
+                    for (var i = 0; i < data.body.transrecordList.length; i++) {
+                        var str = data.body.transrecordList[i].createTime;
+                        var time = str.split(" ");
+                        var hours = time[1].split(":")[0];
+                        var appType = data.body.transrecordList[i].appType;
+
+                        var status = data.body.transrecordList[i].status;
+
+                        Cycle += "<li><div style='width:20% '>";
+                        if (hours < 12) {
+                            Cycle += "<span class='morning'>上午</span>";
+                        } else {
+                            Cycle += "<span class='morning'>下午</span>";
+                        }
+                        Cycle += "<span>" + time[1] + "</span></div><div style='width:25% '>";
+                        if (appType == 1000) {
+                            Cycle += "<h4>话费充值</h4>";
+                        } else {
+                            Cycle += "<h4>话费流量</h4>";
+                        }
+                        Cycle += "</div><div style='width:41% '><span>-" + data.body.transrecordList[i].realAmt + "</span> <span style='font-size: 0.9rem;line-height: 1rem;'>给" + data.body.transrecordList[i].destNo + "充值</span></div> "
+                        if (status == 0) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color:#999;white-space:nowrap'>充值</br>失败</span></div>";
+                        } else if (status == 1) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>充值</br>成功</span></div>";
+                        } else if (status == 2) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>等待</br>充值</span></div>";
+                        }
+                        if (status == 3) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>充值</br>中</span></div>";
+                        }
+                        if (status == 4) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销</br>审核中</span></div>";
+                        }
+                        if (status == 5) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销中</span></div>";
+                        }
+                        if (status == 6) {
+                            Cycle += "<div style='width: 7%'><span class='Result' style='color: #55a532;white-space:nowrap'>返销</br>成功</span></div>";
+                        }
+                        Cycle += "</li>"
+                    }
+                    $(".calendarListY").empty().append(Cycle);
+                }
+            },
+            error: function (data) {	//失败回调函数
+                console.log(data);
+            }
+        });
+    });
+   /*$('#demo1').bind('input propertychange', function () {
+
+         var dateL = $(this).val().replace(/年/, "").replace(/月/, "").replace(/日/, "");
+        // console.log(date);
+         //var mobile=num[1].split("&");
+         //var memberId =mobile[0];
+         //var transDate = dateL;
+       var memberId = 'bwgzydVRnlfJjGmJ';
+       var transDate = "20171027";
+         var date = {transDate: transDate, memberId: memberId};
+         $.ajax({
+             url: BaseURL + "/getTransrecords",
+             type: 'post',
+             data: date,
+             dataType: "jsonp",
+             jsonpCallback: "success_jsonpCallback",
+             contentType: 'application/jsonp;charset=utf-8',
+             success: function (data) {	//成功回调函数
+                // console.log(data);
+                 var  Total=data.body.totalMoney;
+                 $("#Total").html(Total);
+                 var Cycle = "";
+                 for (var i = 0; i < data.body.transrecordList.length; i++) {
+                     var str=data.body.transrecordList[i].createTime;
+                     var  time=str.split(" ");
+                     var hours=time[1].split(":")[0];
+                     var appType=data.body.transrecordList[i].appType;
+                     var status=data.body.transrecordList[i].status;
+                     Cycle+="<li><div style='width:20% '>";
+                     if(hours<12){
+                         Cycle+="<span>上午</span>"
+                     }else {
+                         Cycle+="<span>下午</span>"
+                     }
+                     Cycle+="<span>"+time[1]+"</span></div><div style='width:25% '>";
+                     if(appType==1000){
+                         Cycle+="<h4>话费充值</h4></div>";
+                     }else {
+                         Cycle+="<h4>话费充值</h4></div>";
+                     }
+
+                     if(data.body.transrecordList[i].status==1 || data.body.transrecordList[i].status==0){
+                         Cycle+="<div style='width:40% '><span>-"+data.body.transrecordList[i].realAmt+"</span><span style='font-size: 0.9rem;line-height: 1rem;'>给"+data.body.transrecordList[i].destNo+"充值</span></div>"
+
+                     }else {
+                         Cycle+="<div style='width:40% '><span style='opacity: 0;'>00</span><span style='font-size: 0.9rem;line-height: 1rem;'>给"+data.body.transrecordList[i].destNo+"充值</span></div>"
+
+                     }
+                     if(status==0){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color:#999'>充值失败</span></div></li>";
+                     }else if(status==1){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>充值成功</span></div></li>";
+                     }else if(status==2){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>等待充值</span></div></li>";
+                     }if(status==3){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>充值中</span></div></li>";
+                     }if(status==4){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销审核中</span></div></li>";
+                     }if(status==5){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销中</span></div></li>";
+                     }if(status==6){
+                         Cycle+="<div style='width: 7%'><span class='Result' style='color: #55a532'>返销成功</span></div></li>";
+                     }
+                 }
+                 $(".calendarList").empty().append(Cycle);
+             },
+             error: function (data) {	//失败回调函数
+                 console.log(data)
+             }
+         });
+     });*/
+});
+function p(s) {
+    return s < 10 ? '0' + s : s;
+}
